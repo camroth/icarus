cf9ecc08ac8dbbd78a8bcbeacfeb70099fbef55b
Cameron Roth <camroth@fb.com>
2020-08-10 19:13:47 -0700

    Initial Commit

---
+++ Icarus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Icarus
+//
+//  Created by Cameron Roth on 8/10/20.
+//  Copyright © 2020 Cameron Roth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
