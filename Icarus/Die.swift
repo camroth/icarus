a2a1ec2afc17a40caa3a69a70536a4397f33e69b
Cameron Roth <camroth@fb.com>
2020-08-10 19:32:04 -0700

    Add Die object that defaults to 6 sides

---
+++ Icarus/Die.swift
@@ -0,0 +1,16 @@
+//
+//  Die.swift
+//  Icarus
+//
+//  Created by Cameron Roth on 8/10/20.
+//  Copyright © 2020 Cameron Roth. All rights reserved.
+//
+
+import Foundation
+
+/// A die to be rolled that will return a random number between 1 and the number of sides
+public final class Die {
+
+  /// The number of sides the die can have.
+  public let numberOfSides: NSInteger = 6
+}
